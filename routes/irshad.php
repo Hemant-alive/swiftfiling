afb3dcf53932d9f1da624a289f17c23d01c86f7a
unknown <Irshad@dfsgdh.alivenetsolutions.com>
2017-10-30 13:52:50 +0530

    add new file irshad.php

---
+++ routes/irshad.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//irshad master
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//admin login
+
+Route::get('/admin', function () {
+    return view('admin/login');
+});
+Auth::routes();
+Route::get('admin/dashboard', 'admin\DashboardController@index');
+
+# Admin Panel Routes
+Route::get('admin/users/create', 'UserController@create');
+Route::post('admin/store', 'UserController@store');
+
+Route::get('admin/userList', 'UserController@index');
+/*Route::get('admin/users/{id}/edit', 'UserController@edit');*/
+//Route::get('admin/user/status/{id}', 'RegistrationController@user_active_Inactive');
+//Route::resource('admin/users', 'RegistrationController');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home1', 'HomeController@index')->name('home');
+Route::get('/irshad30', 'HomeController@irshad27')->name('irshad30');
